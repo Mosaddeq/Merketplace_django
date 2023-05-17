a9a25541a7931344be7b8bd68e023118ebf3842c
Shadman Mosaaddeq <shadmanmosaddeq@gmail.com>
2023-05-17 15:03:36 +0600

    atp1

--- marketplace/core/forms.py
+++ marketplace/core/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
+]
+
 #for logged in users
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -13,12 +20,17 @@ class LoginForm(AuthenticationForm):
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username','email','password1','password2')
+        fields = ('username','email','gender','password1','password2')
 
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your Username',
         'class': 'w-full px-6 py-4 rounded-xl'
     }))
+    gender = forms.CharField(widget=forms.Select(choices=GENDER_CHOICES, attrs={
+        'placeholder': 'Select your gender',
+        'class': 'w-full px-6 py-4 rounded-xl',
+    }))
+
 
     email = forms.CharField(widget=forms.EmailInput(attrs={
         'placeholder': "Your email adress",
